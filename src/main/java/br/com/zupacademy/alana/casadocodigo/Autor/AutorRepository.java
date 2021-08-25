631b9a3aa3fe94eb1ce3a009cf0055611103144e
AlanaZUP <alana.reis@zup.com.br>
2021-08-25 11:49:08 -0300

    Repository Aluno

--- src/main/java/br/com/zupacademy/alana/casadocodigo/Autor/AutorRepository.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Autor/AutorRepository.java
@@ -1,2 +1,8 @@
-package br.com.zupacademy.alana.casadocodigo.Autor;public interface AutorRepository {
+package br.com.zupacademy.alana.casadocodigo.Autor;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends CrudRepository<Autor, Long> {
 }
