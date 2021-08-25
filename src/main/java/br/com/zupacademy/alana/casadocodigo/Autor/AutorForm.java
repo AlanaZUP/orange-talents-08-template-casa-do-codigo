bed6f67cf7a66c38c8494e0f07b97bd438602393
AlanaZUP <alana.reis@zup.com.br>
2021-08-25 11:48:12 -0300

    DTO request Aluno

--- src/main/java/br/com/zupacademy/alana/casadocodigo/Autor/AutorForm.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Autor/AutorForm.java
@@ -10,12 +10,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
-@Entity
-public class Autor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class AutorForm {
     @NotEmpty
     private String nome;
 
@@ -25,12 +20,23 @@ public class Autor {
     @NotEmpty @Length(max = 400)
     private String descricao;
 
-    private LocalDateTime instanteCadastro;
 
-    public Autor(String nome, String email, String descricao) {
+    public AutorForm(String nome, String email, String descricao) {
         this.nome = nome;
         this.email = email;
         this.descricao = descricao;
-        this.instanteCadastro = LocalDateTime.now();
+    }
+
+    public Autor converterParaAutor(){
+        return new Autor(this.nome, this.email, this.descricao);
+    }
+
+    @Override
+    public String toString() {
+        return "AutorForm{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
     }
 }
