c4cf09d1f93eb5cdb89c0379b53e251267ed13dd
AlanaZUP <alana.reis@zup.com.br>
2021-08-25 11:48:31 -0300

    DTO response Aluno

--- src/main/java/br/com/zupacademy/alana/casadocodigo/Autor/AutorDTO.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Autor/AutorDTO.java
@@ -10,27 +10,42 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
-@Entity
-public class Autor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class AutorDTO {
     private Long id;
 
-    @NotEmpty
     private String nome;
 
-    @NotEmpty @Email
     private String email;
 
-    @NotEmpty @Length(max = 400)
     private String descricao;
 
     private LocalDateTime instanteCadastro;
 
-    public Autor(String nome, String email, String descricao) {
-        this.nome = nome;
-        this.email = email;
-        this.descricao = descricao;
-        this.instanteCadastro = LocalDateTime.now();
+    public AutorDTO(Autor autor) {
+        this.id = autor.getId();
+        this.nome = autor.getNome();
+        this.email = autor.getEmail();
+        this.descricao = autor.getDescricao();
+        this.instanteCadastro = autor.getInstanteCadastro();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDateTime getInstanteCadastro() {
+        return instanteCadastro;
     }
 }
