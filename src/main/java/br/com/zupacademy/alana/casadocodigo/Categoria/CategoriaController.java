1ece794e7d792bd7551cd57fed7e2883b1fa8392
AlanaZUP <alana.reis@zup.com.br>
2021-08-26 19:14:29 -0300

    Controller Categoria

--- src/main/java/br/com/zupacademy/alana/casadocodigo/Categoria/CategoriaController.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Categoria/CategoriaController.java
@@ -1,2 +1,28 @@
-package br.com.zupacademy.alana.casadocodigo.Categoria;public class CategoriaController {
+package br.com.zupacademy.alana.casadocodigo.Categoria;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<CategoriaDTO> adicionarCategoria(@RequestBody @Valid CategoriaForm categoriaForm){
+        Categoria categoria = categoriaForm.converterParaCategoria();
+        manager.persist(categoria);
+        return ResponseEntity.ok().body(new CategoriaDTO(categoria));
+    }
 }
