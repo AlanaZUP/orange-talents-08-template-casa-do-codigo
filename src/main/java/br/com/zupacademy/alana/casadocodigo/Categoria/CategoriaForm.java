6da836653ebcf36a05c17cda13f89f0426ba9086
AlanaZUP <alana.reis@zup.com.br>
2021-08-26 19:13:05 -0300

    DTO Request Categoria

--- src/main/java/br/com/zupacademy/alana/casadocodigo/Categoria/CategoriaForm.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Categoria/CategoriaForm.java
@@ -1,29 +1,29 @@
 package br.com.zupacademy.alana.casadocodigo.Categoria;
 
 import br.com.zupacademy.alana.casadocodigo.Validators.AnotacoesPersonalizadas.ValorUnico;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
-public class Categoria {
+public class CategoriaForm {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @ValorUnico(nomeCampo = "nome", classe = Categoria.class)
     @NotEmpty
     private String nome;
 
-    public Categoria(@NotEmpty String nome) {
-        this.nome = nome;
-    }
+//    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+//    public CategoriaForm(@NotEmpty String nome) {
+//        this.nome = nome;
+//    }
 
-    public Long getId() {
-        return id;
+    public Categoria converterParaCategoria(){
+        return new Categoria(this.nome);
     }
 
     public String getNome() {
         return nome;
     }
-
 }
