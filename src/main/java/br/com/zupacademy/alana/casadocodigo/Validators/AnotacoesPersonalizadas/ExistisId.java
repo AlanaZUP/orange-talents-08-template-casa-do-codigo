d27ce166f20bd7a413a6341c1825e86f71500a1c
AlanaZUP <alana.reis@zup.com.br>
2021-08-27 17:39:02 -0300

    Interface de anotação se o id existe

--- src/main/java/br/com/zupacademy/alana/casadocodigo/Validators/AnotacoesPersonalizadas/ExistisId.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Validators/AnotacoesPersonalizadas/ExistisId.java
@@ -1,2 +1,19 @@
-package br.com.zupacademy.alana.casadocodigo.Validators.AnotacoesPersonalizadas;public @interface ExistisId {
+package br.com.zupacademy.alana.casadocodigo.Validators.AnotacoesPersonalizadas;
+
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {ExistIdValidator.class})
+public @interface ExistisId {
+    String message() default "Não existe registro com esse campo de identificação";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default { };
+    Class<?> classe();
 }
