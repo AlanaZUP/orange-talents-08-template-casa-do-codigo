ae4cc9046b7bf16023653da98d560ad639f28ddb
AlanaZUP <alana.reis@zup.com.br>
2021-08-30 13:09:19 -0300

    Dto Pais para o request

--- src/main/java/br/com/zupacademy/alana/casadocodigo/paises/PaisRequest.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/paises/PaisRequest.java
@@ -1,2 +1,19 @@
-package br.com.zupacademy.alana.casadocodigo.paises;public class PaisRequest {
+package br.com.zupacademy.alana.casadocodigo.paises;
+
+import br.com.zupacademy.alana.casadocodigo.validators.AnotacoesPersonalizadas.CampoUnico;
+
+import javax.validation.constraints.NotEmpty;
+
+public class PaisRequest {
+
+    @NotEmpty @CampoUnico(nomeCampo = "nome", classe = Pais.class)
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Pais toModel(){
+        return new Pais(nome);
+    }
 }
