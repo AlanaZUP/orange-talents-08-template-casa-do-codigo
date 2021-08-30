0564a77482dd6db74809654fcd0456ecc51c969c
AlanaZUP <alana.reis@zup.com.br>
2021-08-30 14:57:37 -0300

    DTO estado request

---
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/estado/EstadoRequest.java
@@ -0,0 +1,32 @@
+package br.com.zupacademy.alana.casadocodigo.estado;
+
+import br.com.zupacademy.alana.casadocodigo.estadoTeste.Estado;
+import br.com.zupacademy.alana.casadocodigo.paises.Pais;
+import br.com.zupacademy.alana.casadocodigo.paises.PaisRepository;
+import br.com.zupacademy.alana.casadocodigo.validators.AnotacoesPersonalizadas.ExistisId;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class EstadoRequest {
+
+    @NotEmpty
+    private String nome;
+
+    @NotNull
+    @ExistisId(classe = Pais.class)
+    private Long idPais;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getIdPais() {
+        return idPais;
+    }
+
+    public br.com.zupacademy.alana.casadocodigo.estadoTeste.Estado toModel(PaisRepository paisRepository){
+        Pais pais = paisRepository.findById(idPais).get();
+        return new Estado(nome, pais);
+    }
+}
