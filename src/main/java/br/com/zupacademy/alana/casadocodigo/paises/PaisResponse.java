6701ebb9fc91a306dda6fff42cc4ec9973e75bf7
AlanaZUP <alana.reis@zup.com.br>
2021-08-30 13:09:36 -0300

    Dto Pais para o response

--- src/main/java/br/com/zupacademy/alana/casadocodigo/paises/PaisResponse.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/paises/PaisResponse.java
@@ -1,2 +1,20 @@
-package br.com.zupacademy.alana.casadocodigo.paises;public class PaisResponse {
+package br.com.zupacademy.alana.casadocodigo.paises;
+
+public class PaisResponse {
+
+    private Long id;
+    private String nome;
+
+    public PaisResponse(Pais pais) {
+        this.id = pais.getId();
+        this.nome = pais.getNome();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
