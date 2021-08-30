e8a3677489282d581254b77f6cdaaa9bff372e72
AlanaZUP <alana.reis@zup.com.br>
2021-08-30 09:10:45 -0300

    Livro Repository

---
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Livro/LivroDTOList.java
@@ -0,0 +1,78 @@
+package br.com.zupacademy.alana.casadocodigo.Livro;
+
+import br.com.zupacademy.alana.casadocodigo.Autor.Autor;
+import br.com.zupacademy.alana.casadocodigo.Categoria.Categoria;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class LivroDTO {
+
+    private Long id;
+    private String titulo;
+    private String resumo;
+    private String sumario;
+    private BigDecimal preco;
+    private int numeroPaginas;
+    private String isbn;
+    private LocalDate dataPublicacao;
+    private Categoria categoria;
+    private Autor autor;
+
+    public LivroDTO(Livro livro) {
+        this.id = livro.getId();
+        this.titulo = livro.getTitulo();
+        this.resumo = livro.getResumo();
+        this.sumario = livro.getSumario();
+        this.preco = livro.getPreco();
+        this.numeroPaginas = livro.getNumeroPaginas();
+        this.isbn = livro.getIsbn();
+        this.dataPublicacao = livro.getDataPublicacao();
+        this.categoria = livro.getCategoria();
+        this.autor = livro.getAutor();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public String getSumario() {
+        return sumario;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+}
---
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Livro/LivroRepository.java
@@ -0,0 +1,6 @@
+package br.com.zupacademy.alana.casadocodigo.Livro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
