35fe37418f2ee4ecede3b550801b87520f4eb559
AlanaZUP <alana.reis@zup.com.br>
2021-08-30 09:11:54 -0300

    DTO Livros para exibir apenas id e titulo

--- src/main/java/br/com/zupacademy/alana/casadocodigo/Livro/LivroDTOList.java
+++ src/main/java/br/com/zupacademy/alana/casadocodigo/Livro/LivroDTOList.java
@@ -2,38 +2,18 @@ package br.com.zupacademy.alana.casadocodigo.Livro;
 
 import br.com.zupacademy.alana.casadocodigo.Autor.Autor;
 import br.com.zupacademy.alana.casadocodigo.Categoria.Categoria;
-import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.*;
-import javax.validation.Valid;
-import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class LivroDTO {
+public class LivroDTOList {
 
     private Long id;
     private String titulo;
-    private String resumo;
-    private String sumario;
-    private BigDecimal preco;
-    private int numeroPaginas;
-    private String isbn;
-    private LocalDate dataPublicacao;
-    private Categoria categoria;
-    private Autor autor;
 
-    public LivroDTO(Livro livro) {
+    public LivroDTOList(Livro livro) {
         this.id = livro.getId();
         this.titulo = livro.getTitulo();
-        this.resumo = livro.getResumo();
-        this.sumario = livro.getSumario();
-        this.preco = livro.getPreco();
-        this.numeroPaginas = livro.getNumeroPaginas();
-        this.isbn = livro.getIsbn();
-        this.dataPublicacao = livro.getDataPublicacao();
-        this.categoria = livro.getCategoria();
-        this.autor = livro.getAutor();
     }
 
     public Long getId() {
@@ -42,37 +22,5 @@ public class LivroDTO {
 
     public String getTitulo() {
         return titulo;
-    }
-
-    public String getResumo() {
-        return resumo;
-    }
-
-    public String getSumario() {
-        return sumario;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public int getNumeroPaginas() {
-        return numeroPaginas;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public LocalDate getDataPublicacao() {
-        return dataPublicacao;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public Autor getAutor() {
-        return autor;
     }
 }
